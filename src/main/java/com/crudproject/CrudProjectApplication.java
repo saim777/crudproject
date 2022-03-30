00904a713a00f940f18d7e2327a1e0205cdff454
saim777 <rken775@gmail.com>
2022-03-30 21:01:15 +0530

    Initial Commit

--- src/main/java/com/crudproject/CrudProjectApplication.java
+++ src/main/java/com/crudproject/CrudProjectApplication.java
@@ -8,5 +8,6 @@ public class CrudProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudProjectApplication.class, args);
+	//	System.out.println("Test");
 	}
 }
