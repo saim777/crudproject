0539ceb63c4c9e56cad7a4323c651189c16ecaa4
saim777 <rken775@gmail.com>
2022-03-30 21:03:43 +0530

    Uncomment file

--- src/main/java/com/crudproject/CrudProjectApplication.java
+++ src/main/java/com/crudproject/CrudProjectApplication.java
@@ -8,6 +8,6 @@ public class CrudProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudProjectApplication.class, args);
-	//	System.out.println("Test");
+	    System.out.println("Test");
 	}
 }
